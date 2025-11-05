d9d8c2f1a87d078f46b2384478e8f47b7d21d2d3
haruhisayaguchi <hal.yaguchi@gmail.com>
2025-11-04 20:53:45 -0800

    WAD201_W5D2 completed exercise

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 // YOUR CODE HERE
+document.querySelector('#btn1').addEventListener('click', () => {
+	document.querySelector('#output1').style.color = 'red';
+})
+
+document.querySelector('#btn2').addEventListener('click', () => {
+	const span = document.createElement('span')
+	span.textContent = 'Hello World'
+	document.querySelector('#output2').append(span)
+})
+
+document.querySelector('#btn3').addEventListener('click', () => {
+	document.querySelector('#output3').innerHTML = ''
+})
+
+document.querySelector('#btn4').addEventListener('click', () => {
+	document
+		.querySelector('#output4')
+		.querySelectorAll('p')
+		.forEach(p => p.style.color = 'red')
+})
+
+document.querySelector('#btn5').addEventListener('click', () => {
+	const inputValue = document
+		.querySelector('#message')
+		.value;
+	console.log(inputValue)
+})
